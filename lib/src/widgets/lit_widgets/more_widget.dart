1dfece6f8dd35ed90ce51dd5511f24aed703d86c
Thu Rain Hein <thurainhein097@gmail.com>
2025-05-15 17:27:37 +0630

    Add LoadMoreUiBuilder for v0.1.5 main bra

--- lib/src/widgets/lit_widgets/more_widget.dart
+++ lib/src/widgets/lit_widgets/more_widget.dart
@@ -1,13 +1,13 @@
-import 'package:flutter/material.dart';
-
-class MoreWidget extends StatelessWidget {
-  // Non-final field causing must_be_immutable warning
-
-  @override
-  Widget build(BuildContext context) {
-    // Example rendering for "No more data" or load more UI
-    return Center(
-      child: Text("No more data found.\""), // Unnecessary escape in string
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class MoreWidget extends StatelessWidget {
+//   // Non-final field causing must_be_immutable warning
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // Example rendering for "No more data" or load more UI
+//     return Center(
+//       child: Text("No more data found.\""), // Unnecessary escape in string
+//     );
+//   }
+// }
