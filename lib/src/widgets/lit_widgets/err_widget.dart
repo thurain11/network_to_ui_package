53a3fd5ccf908b39980aa780612e5a255c9f9ef3
Thu Rain Hein <thurainhein097@gmail.com>
2025-05-15 17:17:03 +0630

    Add LoadMoreUiBuilder for v0.1.5 main branc

--- lib/src/widgets/lit_widgets/err_widget.dart
+++ lib/src/widgets/lit_widgets/err_widget.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import '../../utils/response_ob.dart'; // Assuming ErrState is defined here
 
 class ErrWidget extends StatelessWidget {
-  ErrState errState; // Non-final field causing warning
-  VoidCallback? func; // Non-final field causing warning
+  final ErrState errState; // Non-final field causing warning
+  final VoidCallback? func; // Non-final field causing warning
 
-  ErrWidget({Key? key, required this.errState, this.func}) : super(key: key);
+  const ErrWidget({super.key, required this.errState, this.func});
 
   @override
   Widget build(BuildContext context) {
